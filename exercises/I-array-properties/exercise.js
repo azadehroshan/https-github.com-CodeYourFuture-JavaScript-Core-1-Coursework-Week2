728a9c75e82e4c996bebd5b36eaf347c3bcccaca
azadehroshan <105980239+azadehroshan@users.noreply.github.com>
2022-09-12 14:58:19 +0100

    Initial commit

---
+++ exercises/I-array-properties/exercise.js
@@ -0,0 +1,26 @@
+/*
+  Array properites
+  ----------------
+  Complete the function to test if an array is empty (has no values in it)
+
+*/
+
+function isEmpty(arr) {
+  return; // complete this statement
+}
+
+/* 
+  DO NOT EDIT BELOW THIS LINE
+  --------------------------- */
+let numbers = [1, 2, 3];
+let names = [];
+
+console.log(isEmpty(numbers));
+console.log(isEmpty(names));
+
+/* 
+  EXPECTED RESULT
+  ---------------
+  false
+  true
+*/
