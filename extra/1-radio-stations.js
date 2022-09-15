327541fe925d312d471a367325360bda709eacea
azadehroshan <105980239+azadehroshan@users.noreply.github.com>
2022-09-15 16:48:44 +0100

    extra complated

--- extra/1-radio-stations.js
+++ extra/1-radio-stations.js
@@ -46,9 +46,21 @@ function getAvailableStations() {
     getAvailableStations.stations.sort(function (frequencyA, frequencyB) {
       return frequencyA - frequencyB;
     });
-  }
-
+  } 
+  
   return getAvailableStations.stations;
+}
+
+function getStations(){
+  return getAvailableStations();
+}
+ 
+
+function getAllFrequencies(){ 
+  return [
+    87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104,
+    105, 106, 107, 108,
+  ];
 }
 
 function isRadioStation(frequency) {
